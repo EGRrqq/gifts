e1ca4bb57f64831ccd6dcab0117d7b95876384fb
EGRrqq <egrrqqdev@gmail.com>
2024-05-04 04:58:54 -0400

    feat(schema): make sales TEXT fields required
    feat(schema): reoder sales rows

--- server/src/schemas/sales/sales.sql
+++ server/src/schemas/sales/sales.sql
@@ -1,10 +1,10 @@
 CREATE TABLE sales (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  number_of_gifts INT NOT NULL,
   gift_card_id INT NOT NULL,
+  number_of_gifts INT NOT NULL,
   day_to_claim_gift INT NOT NULL,
-  description TEXT,
-  card_numbers TEXT,
+  description TEXT NOT NULL,
+  card_numbers TEXT NOT NULL,
   FOREIGN KEY (gift_card_id) REFERENCES gift_cards(id)
 );
