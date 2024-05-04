d2e845e228df28b2a2e0cf638778b913de14a4a6
EGRrqq <egrrqqdev@gmail.com>
2024-05-04 06:37:08 -0400

    feat: rename sales service to sale

--- server/src/controllers/sales.controller.ts
+++ server/src/controllers/sales.controller.ts
@@ -1,10 +1,10 @@
 import { TExpressParams } from "./TExpressParams";
-import * as sales from "../services/sales.service";
+import * as sale from "../services/sales.service";
 import { Sale } from "../models";
 
 export const get: TExpressParams = async (_, res, next) => {
   try {
-    const data = await sales.getAll();
+    const data = await sale.getAll();
     res.json(data);
   } catch (err) {
     const message = "Error while getting sales";
@@ -18,12 +18,54 @@ export const get: TExpressParams = async (_, res, next) => {
 
 export const create: TExpressParams = async (req, res, next) => {
   try {
-    const data = await sales.create(new Sale(req));
+    const data = await sale.create(new Sale(req));
     res.json(data);
   } catch (err) {
     const message = "Error while creating Sale";
 
     res.status(500).json({
+      message: err.message || message,
+    });
+    next(err);
+  }
+};
+
+export const getSingle: TExpressParams = async (req, res, next) => {
+  try {
+    const data = await sale.findById(parseInt(req.params.id));
+    res.json(data);
+  } catch (err) {
+    const message = `Error while getting Sale with id ${req.params.id}.`;
+
+    res.status(err.code || 500).json({
+      message: err.message || message,
+    });
+    next(err);
+  }
+};
+
+export const update: TExpressParams = async (req, res, next) => {
+  try {
+    const data = await sale.update(parseInt(req.params.id), new Sale(req));
+    res.json(data);
+  } catch (err) {
+    const message = "Error while updating Sale";
+
+    res.status(500).json({
+      message: err.message || message,
+    });
+    next(err);
+  }
+};
+
+export const remove: TExpressParams = async (req, res, next) => {
+  try {
+    const data = await sale.remove(parseInt(req.params.id));
+    res.json(data);
+  } catch (err) {
+    const message = "Error while deleted Sale";
+
+    res.status(err.code || 500).json({
       message: err.message || message,
     });
     next(err);
