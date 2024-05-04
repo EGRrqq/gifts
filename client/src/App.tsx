e3c95813f7a6146d698c55d8b4d3f3d6f403b169
EGRrqq <egrrqqdev@gmail.com>
2024-05-04 10:30:07 -0400

    feat: add material ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from "react";
-
-interface IData {
-  buh: string;
-}
+import { Button } from "@mui/material";
 
 function App() {
-  const [data, setData] = useState<IData | null>();
-
-  useEffect(() => {
-    const link = import.meta.env.PROD ? import.meta.env.VITE_API_LINK : "./api";
-
-    fetch(link + "/buh")
-      .then((res) => res.json())
-      .then((data: IData) => setData(data));
-  }, []);
-
   return (
     <>
-      <h1>{data?.buh}</h1>
+      <Button variant="contained">bruh</Button>
     </>
   );
 }
