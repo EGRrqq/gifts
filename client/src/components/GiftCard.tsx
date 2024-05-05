2dd6a1db51d89e94096922154b2e0da32c8c2b19
EGRrqq <egrrqqdev@gmail.com>
2024-05-05 09:11:47 -0400

    feat: remove useless imports from giftCard component

--- client/src/components/GiftCard.tsx
+++ client/src/components/GiftCard.tsx
@@ -1,14 +1,7 @@
-import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-// import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
-// import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-// import SkipNextIcon from "@mui/icons-material/SkipNext";
-
 import { IGiftCard } from "../redux/giftCard/model/types";
 import { Divider, Stack } from "@mui/material";
 import { formatDate } from "../helpers";
@@ -18,17 +11,8 @@ interface IGiftCardProps {
 }
 
 export default function GiftCard({ card }: IGiftCardProps) {
-  const theme = useTheme();
-  console.log(theme.direction);
-
   return (
     <Card sx={{ display: "flex" }}>
-      {/* <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image="/static/images/cards/live-from-space.jpg"
-        alt="Live from space album cover"
-      /> */}
       <Box sx={{ display: "flex", flexDirection: "column", flex: 1 }}>
         <CardContent sx={{ flex: "1 0 auto" }}>
           <Typography component="div" variant="h5">
