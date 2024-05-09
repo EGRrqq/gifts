e4b7f7310616fc76e48ca12adc0f7b5b287e8dcc
EGRrqq <egrrqqdev@gmail.com>
2024-05-09 09:53:20 -0400

    feat: turn sales data into table

--- client/src/components/table/models/Sale.tsx
+++ client/src/components/table/models/Sale.tsx
@@ -12,6 +12,16 @@ import {
 import { ISale } from "../../../redux/sale/model/interfaces";
 import * as saleActions from "../../../redux/sale/actions";
 
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button, IconButton } from "@mui/material";
+import RemoveIcon from "@mui/icons-material/Remove";
+
 const mapStateToProps = createMapStateToProps<ISale>(
   (state) => state.sale.sales
 );
@@ -24,14 +34,50 @@ const Sale = ({ boundData, data }: SaleProps) => {
   }, [boundData]);
 
   return (
-    <>
-      {data.map((s) => (
-        <section key={s.id}>
-          <h1>Name: {s.name}</h1>
-          <p>number_of_gifts: {s.number_of_gifts}</p>
-        </section>
-      ))}
-    </>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Название рассылки</TableCell>
+            <TableCell align="center">Дата рассылки</TableCell>
+            <TableCell align="center">Кол-во отправленных подарков</TableCell>
+            <TableCell align="center">Отмена рассылки</TableCell>
+            <TableCell align="center">Редактировать рассылку</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((d) => (
+            <TableRow
+              key={d.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell align="center" component="th" scope="row">
+                {d.name}
+              </TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center"></TableCell>
+              <TableCell align="center">
+                <IconButton
+                  style={{ backgroundColor: "gray", borderRadius: 0 }}
+                  aria-label="delete"
+                  size="medium"
+                >
+                  <RemoveIcon
+                    style={{
+                      color: "white",
+                      background: "transparent",
+                      borderRadius: 0,
+                    }}
+                    fontSize="inherit"
+                  />
+                </IconButton>
+              </TableCell>
+              <TableCell align="center"></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
