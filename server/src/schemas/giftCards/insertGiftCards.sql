cc4d2f51c2dacfdc083b4f6d32c32015a5cbda12
EGRrqq <egrrqqdev@gmail.com>
2024-06-12 03:41:45 -0400

    feat: update expiration date for gift cards
    cause old gift cards are expired
    and
    init "db init-cfg" shows expired message

--- server/src/schemas/giftCards/insertGiftCards.sql
+++ server/src/schemas/giftCards/insertGiftCards.sql
@@ -1,27 +1,27 @@
 INSERT INTO gift_cards 
 (name, remaining_quantity, expiration_date, denomination)
 VALUES 
-("KFC", 250, "2024-12-31", 1000);
+("KFC", 250, "2025-12-31", 1000);
 
 INSERT INTO gift_cards 
 (name, remaining_quantity, expiration_date, denomination)
 VALUES 
-("McDonalds", 122, "2024-12-31", 1200);
-
-
-INSERT INTO gift_cards 
-(name, remaining_quantity, expiration_date, denomination)
-VALUES 
-("Nike", 25, "2024-08-31", 2500);
+("McDonalds", 122, "2025-12-31", 1200);
 
 
 INSERT INTO gift_cards 
 (name, remaining_quantity, expiration_date, denomination)
 VALUES 
-("Puma", 76, "2024-08-31", 2200);
+("Nike", 25, "2025-08-31", 2500);
+
 
 INSERT INTO gift_cards 
 (name, remaining_quantity, expiration_date, denomination)
 VALUES 
-("Amazon", 120, "2024-05-31", 500);
+("Puma", 76, "2025-08-31", 2200);
+
+INSERT INTO gift_cards 
+(name, remaining_quantity, expiration_date, denomination)
+VALUES 
+("Amazon", 120, "2025-05-31", 500);
 
